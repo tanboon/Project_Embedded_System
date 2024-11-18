ddca101c50cdcfb1516665aa1d8017c6cc938eea
Tanboon Tiengtut <tanboon.top3837@gmail.com>
2024-11-18 22:58:00 +0700

    initial project, setup TailwindCSS

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# Project_Embedded_System
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-lg text-green-500">Test TailwindCSS</h1>
+        <Typography>Test</Typography>
+      </header>
+    </div>
+  );
+}
+
+export default App;
